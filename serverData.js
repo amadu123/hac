745d1bc205184466ae9528a73fc31e80a402a70c
amadu123 <58538816+amadu123@users.noreply.github.com>
2021-12-01 15:26:17 -0500

    Update serverData.js

--- serverData.js
+++ serverData.js
@@ -1,1 +1,1 @@
-window.vultr={"scheme":"mm_exp","servers":[{"ip":"_","scheme":"mm_exp","region":"vultr:1","index":0,"games":[{"playerCount":666,"isPrivate":false}]}]}
+window.vultr={"scheme":"mm_exp","servers":[{"ip":"_","scheme":"mm_exp","region":"vultr:90","index":0,"games":[{"playerCount":0,"isPrivate":true}]}]}
